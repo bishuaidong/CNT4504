4af117f9b15bae0ca07ca91dc19aed81a30d5d49
clayj121 <36213267+clayj121@users.noreply.github.com>
2018-03-02 11:54:18 -0500

    Update Client.java

--- Project1/src/project1/Client.java
+++ Project1/src/project1/Client.java
@@ -5,6 +5,7 @@
  */
 package project1;
 
+import java.io.*;
 import java.net.*;
 import java.util.*;
 /**
@@ -12,47 +13,91 @@ import java.util.*;
  * @author Bishuai
  * @author Varsha
  * @author Rishav
- *
+ * @author Clay
+ * @author Danniel
  */
 public class Client {
+    
+    
     public static void main(String[] args) {
-        /*
-        Create socket object. Two parameters are passed, the first is the IP 
-        of the server and the second is the port number
-        */
-        int portNumber;
-        try {
-            System.out.println("Attempting to connect Client");
-            Socket connection = new Socket("localhost", 10095);
-        }
-        catch (Exception e) {
-            System.out.println("Connection unsuccessful");
-        }
-        Scanner in = new Scanner ( System.in );
-        Menu menu = new Menu();
-        String command;
-        boolean running = true;
         
-        while(running) {
-            menu.display_menu();
-            command = menu.getCommand(in.next());
+        
+        int threadCount;
+        ClientThread[] threads;
+        String host;
+        int portNumber = 8080;
+        
+        if (args.length < 1) {
+            System.out.println("Did not enter a host name. Goodbye.");
+        }
+        else {
+            host = args[0];
             
-            if(command.equals("exit")) {
-                System.out.println("Goodbye");
-                running = false;
+            if (args.length == 2) {
+                threadCount = Integer.parseInt(args[1]);
             }
             else {
-                System.out.println("running " + command + "\n");
-               
+                threadCount = 1;
             }
+            
+            
+            
+            Scanner in = new Scanner ( System.in );
+            Menu menu = new Menu();
+            String command;
+            boolean running = true;
+        
+            while(running) {
+                menu.display_menu();
+                command = menu.getCommand(in.next());
+            
+                if(command.equals("exit")) {
+                    System.out.println("Goodbye");
+                    running = false;
+                }
+                else {
+                    System.out.println("running " + command + "\n");
+               
+                }
          
+            }
+        
+            in.close();
         }
         
-        in.close();
     }
 }
 
-public class Menu {
+class ClientThread extends Thread {
+    public double start, end, total;
+    public String command, host;
+    public int port = 8080;
+    
+    public ClientThread(String com, String hos) {
+        command = com;
+        host = hos;
+        total = 0;
+        end = 0;
+        start = 0;
+    }
+    
+    public void execute() {
+        
+    	try {
+    		String line = null;
+            
+            Socket connection = new Socket("localhost", port);
+            PrintWriter output = new PrintWriter(connection.getOutputStream(), true);
+            BufferedReader input = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+    	}
+    	catch (Exception e) {
+    		e.printStackTrace();
+    	}
+        
+    }
+}
+
+class Menu {
     
   public Menu() {
     
