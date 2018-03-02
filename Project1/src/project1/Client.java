364e461101602a5d11db95e23dd996683d32e6e0
clayj121 <36213267+clayj121@users.noreply.github.com>
2018-03-02 09:45:03 -0500

    Update Client.java
    should be fixed

--- Project1/src/project1/Client.java
+++ Project1/src/project1/Client.java
@@ -35,19 +35,20 @@ public class Client {
         
         while(running) {
             menu.display_menu();
-            command = menu.getCommand(in.nextInt());
+            command = menu.getCommand(in.next());
             
             if(command.equals("exit")) {
                 System.out.println("Goodbye");
                 running = false;
             }
-            
             else {
-                System.out.println("running ", command);
+                System.out.println("running " + command + "\n");
+               
             }
+         
         }
         
-        
+        in.close();
     }
 }
 
@@ -58,39 +59,39 @@ class Menu {
   }
     public String getCommand(String input) {
         
-        String command;
+        String command = " ";
         
         switch (input) {
-        case 1:
-            System.out.println ( "current Date and Time" );
+        case "1":
+            System.out.println ( "current Date and Time\n" );
             command = "date";
             break;
-        case 2:
-            System.out.println ( "uptime" );
+        case "2":
+            System.out.println ( "uptime\n" );
             command = "uptime";
             break;
-        case 3:
-            System.out.println ( "memory use" );
+        case "3":
+            System.out.println ( "memory use\n" );
             command = "free";
             break;
-        case 4:
-            System.out.println ( "Netstat" );
+        case "4":
+            System.out.println ( "Netstat\n" );
             command = "netstat";
             break;
-        case 5:
-            System.out.println ( "current users" );
+        case "5":
+            System.out.println ( "current users\n" );
             command = "users";
             break;
-        case 6:
-            System.out.println ( "running processes" );
+        case "6":
+            System.out.println ( "running processes\n" );
             command = "ps aux";
             break;
-        case 7:
-            System.out.println("Quitting.");
+        case "7":
+            System.out.println("Quitting\n");
             command = "exit";
             break;
         default:
-            System.err.println ( "Invalid option." );
+            System.err.println ( "Invalid option\n" );
             break;
     }
       return command;
