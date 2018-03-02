4e89cf019ffcdc62cbf46d80270d2300b5676d10
clayj121 <36213267+clayj121@users.noreply.github.com>
2018-03-02 14:32:03 -0500

    Update Client.java

--- Project1/src/project1/Client.java
+++ Project1/src/project1/Client.java
@@ -20,24 +20,24 @@ public class Client {
     
     
     public static void main(String[] args) {
-        /*
-        Create socket object. Two parameters are passed, the first is the IP 
-        of the server and the second is the port number
-        */
         
-    	int clientCount;
-        
+    	int clientCount, port = 8080;
         String host;
     	
+        //No command line arguments
         if (args.length < 1) {
             System.out.println("Did not enter a host name. Goodbye.");
         }
+        //at least 1 command line argument
         else {
+            //hostname
             host = args[0];
             
+            //user entered number of clients
             if (args.length == 2) {
                 clientCount = Integer.parseInt(args[1]);
             }
+            //default number of clients
             else {
                 clientCount = 1;
             }
@@ -47,18 +47,34 @@ public class Client {
             Menu menu = new Menu();
             String command;
             boolean running = true;
-        
+            
+            //while the user has not exited, display the menu and run the chosen command
             while(running) {
                 menu.display_menu();
                 command = menu.getCommand(in.next());
-            
+                
+                //exit menu
                 if(command.equals("exit")) {
+                    //tells the server to exit
+                    try {
+                		Socket connection = new Socket(host, port);
+                        PrintWriter output = new PrintWriter(connection.getOutputStream(), true);
+                        output.println("exit");
+                        connection.close();
+                	}
+                    catch (Exception e) {
+                    	e.printStackTrace();
+                    }
+                    
+                    //exits the client
                     System.out.println("Goodbye");
                     running = false;
                 }
+                //run the chosen command
                 else {
                     System.out.println("running " + command + "\n");
                     
+                    //creates the thread that will run the chosen command n times
                     ClientThread thread = new ClientThread(command, host, clientCount);
                     thread.start();
                 }
@@ -86,15 +102,35 @@ class ClientThread extends Thread {
     }
     
     public void execute() {
+        System.out.println("Attempting to connect to the server");
         
+        //attempts to connect to the server, run the chosen command n times, and calculates and displays the latency
     	try {
     		String line = null;
             
+            //connects to the server
             Socket connection = new Socket(host, port);
+            System.out.println("Connection Successful");
+            
+            //opens I/O stream
             PrintWriter output = new PrintWriter(connection.getOutputStream(), true);
             BufferedReader input = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            
+            //start timer
+            
+            //run command n times
+            
+            //stop timer
+            
+            //calculate and display latency
+            
+            
+            output.close();
+            input.close();
+            connection.close();
     	}
     	catch (Exception e) {
+            System.out.println("Could not connect to the server");
     		e.printStackTrace();
     	}
         
@@ -103,13 +139,14 @@ class ClientThread extends Thread {
 
 class Menu {
     
-  public Menu() {
+    public Menu() {
     
-  }
+    }
     public String getCommand(String input) {
         
         String command = " ";
         
+        //determines which linux command to run based on the user's input
         switch (input) {
         case "1":
             System.out.println ( "current Date and Time\n" );
@@ -142,11 +179,12 @@ class Menu {
         default:
             System.err.println ( "Invalid option\n" );
             break;
-    }
+        }
       return command;
     }
     
     public void display_menu() {
+        //displays the menu
         System.out.println("The menu provides the following choices to the user: ");
                         System.out.println("1. Host current Date and Time \n2. Host uptime\n"
                                         + "3. Host memory use \n4. Host Netstat \n5. Host current users "
